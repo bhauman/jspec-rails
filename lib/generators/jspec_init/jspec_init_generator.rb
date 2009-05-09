8ac338c279e74737e3b156ba7ce982b54b2ddb36
bhauman <bhauman@gmail.com>
2009-05-09 14:52:06 -0400

    moved jspec_rails.js into generator

--- lib/generators/jspec_init/jspec_init_generator.rb
+++ lib/generators/jspec_init/jspec_init_generator.rb
@@ -3,12 +3,16 @@ class JspecInitGenerator < Rails::Generator::Base
     record do |m|
       m.directory 'jspec'
       m.directory 'jspec/fixtures'
-      m.directory 'jspec/spec'      
+      m.directory 'jspec/spec'
+      m.directory 'public/javascripts'
+      
       m.template 'spec/index.html.erb', File.join('jspec/spec', 'index.html.erb')
       m.template 'spec/jspec_rails_spec.js', File.join('jspec/spec', 'jspec_rails_spec.js')
       m.template 'fixtures/test_fixture.html.erb', File.join('jspec/fixtures', 'test_fixture.html.erb')
       m.template 'jspec.yml', File.join('jspec', 'jspec.yml')
-
+      
+      m.file 'jspec_rails.js', File.join('public/javascripts', 'jspec_rails.js')
+      
       m.directory 'public/jspec'
       %w(jspec.js jspec.jquery.js jspec.css).each do |f|
         m.file File.join('jspec/', f), File.join('public/jspec', f)
