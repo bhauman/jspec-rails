aa7e92e0a421a2f9187832e6a55f99554694e0d8
bhauman <bhauman@gmail.com>
2009-05-11 14:48:13 -0400

    fixed the routes and added fixture caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-#  map.connect 'jspec/:action/:id', :controller => 'jspec'
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.connect 'jspec/:action/:id', :controller => 'jspec'
 end
